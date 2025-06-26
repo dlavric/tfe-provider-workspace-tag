aa2fbb1d3f16c1f14add8a62cb955ef9dc9a8367
dlavric <daniela@hashicorp.com>
2025-06-26 14:13:36 +0200

    modified code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     tfe = {
-      source = "hashicorp/tfe"
+      source  = "hashicorp/tfe"
       version = "0.65.2"
     }
   }
@@ -19,8 +19,10 @@ resource "tfe_organization" "test-organization" {
 resource "tfe_workspace" "test" {
   name         = "tfe-provider-workspace-tag"
   organization = tfe_organization.test-organization.name
-  tags         = {
-      environment = "dev"
-      team_owner = "daniela"
+  tags = {
+    ITBA                     = "test ITBA"
+    correlationId            = "TMA0015224"
+    owningTransactionCycleId = "14"
+    primaryOwner             = "firstname lastname"
   }
 }
